a4a7d70d103dfd61ef29c52ea129cbf7320ab0c9
Joseph <56847381+josephtrandev@users.noreply.github.com>
2021-03-01 12:11:48 -0600

    added temporary business info

--- routes/main_view.js
+++ routes/main_view.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res) {
-    res.render('index', {title: 'hello'});
+    res.render('index', {title: 'Hoof Hearted Stables'});
 });
 
 router.get('/login', function(req,res){
-  res.render('login', {title:'login'});
+  res.render('login', {title:'Hoof Hearted Stables | Login'});
 });
 
 router.get('/signup', function(req,res){
-  res.render('signup', {title:'signup'});
+  res.render('signup', {title:'Hoof Hearted Stables | Signup'});
 });
 
 module.exports = router;
