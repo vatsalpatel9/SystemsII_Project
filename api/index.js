634a1a23ab059c183f600024df1aba3cf1148274
vatsalpatel9 <44619923+vatsalpatel9@users.noreply.github.com>
2021-04-03 22:27:18 -0500

    Refactor Payment Module

--- api/index.js
+++ api/index.js
@@ -5,6 +5,5 @@ router
   .use("/login", require("./login"))
   .use("/signup", require("./signup"))
   .use("/booking", require("./booking"))
-  .use("/process-payment", require("./payment"));
 
 module.exports = router;
