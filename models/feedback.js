35ff2ae02176c415e5f3397504a893eb258f1d60
vatsalpatel9 <44619923+vatsalpatel9@users.noreply.github.com>
2021-04-10 22:25:35 -0500

    Added feedback schema

---
+++ models/feedback.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const feedbackSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+});
+
+const feedback = new mongoose.model("Feedback", feedbackSchema);
+
+module.exports = feedback;
