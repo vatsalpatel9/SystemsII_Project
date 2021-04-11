de31ffa1007cb0d294f44a8707060b02d7383585
vatsalpatel9 <44619923+vatsalpatel9@users.noreply.github.com>
2021-04-11 01:03:22 -0500

    Added Training and Lodging reservation

--- api/booking.js
+++ api/booking.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var moment = require("moment");
-const crypto = require("crypto");
+const SERVICE_TYPE = require("../misc/service_type");
 const Reservation = require("../models/ridingReserve");
+const Training = require("../models/horseTraining");
+const Lodging = require("../models/horseLodging");
 const paymentModel = require("../models/paymentModel");
 const bodyParser = require("body-parser");
 const JSONBig = require("json-bigint");
@@ -9,7 +11,7 @@ require("dotenv").config();
 var router = express.Router();
 
 const { ApiError, Client, Environment } = require("square");
-const { duration } = require("moment");
+const { LODGING } = require("../misc/service_type");
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
@@ -17,12 +19,13 @@ router.use(bodyParser.urlencoded({ extended: true }));
 //--Pass current reservation to payment process
 //////////////////////////////////////////////////////////////////////////////////
 var currId;
-function storeReserveID(id){
+var service;
+function storeReserveID(id) {
   currId = id;
   console.log("currID: " + currId);
 }
 
-function getCurrId(){
+function getCurrId() {
   return currId;
 }
 
@@ -41,7 +44,7 @@ const client = new Client({
 //////////////////////////////////////////////////////////////////////////////////
 //--Post RIDING reservation info to database
 //////////////////////////////////////////////////////////////////////////////////
-router.post('/riding',  async (req, res) => {
+router.post("/riding", async (req, res) => {
   const date = req.body.days;
   const formattedStart = moment(req.body.StartTime, ["h:mm A"]).format("HH:mm");
   const formattedEnd = moment(req.body.EndTime, ["h:mm A"]).format("HH:mm");
@@ -57,42 +60,122 @@ router.post('/riding',  async (req, res) => {
   console.log("Duration: " + duration);
   console.log("price: " + price);
 
-    try{
-        const reserve = new Reservation({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            phone: req.body.phone,
-            numPeople: req.body.numPeople,
-            days: date,
-            startTime: req.body.StartTime,
-            endTime: req.body.EndTime,
-            duration: duration,
-            paymentStatus: false,
-            paymentAmount: price,
-            userId: req.session.user
-        });
-        const reserved = await reserve.save();
-        const reservationId = reserved._id;
-        storeReserveID(reservationId);
-        res.render("customer/payment", {price: price});
-    }catch(error){
-        res.status(400).send(error);
-    }
-
+  try {
+    const reserve = new Reservation({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      phone: req.body.phone,
+      numPeople: req.body.numPeople,
+      days: date,
+      startTime: req.body.StartTime,
+      endTime: req.body.EndTime,
+      duration: duration,
+      paymentStatus: false,
+      paymentAmount: price,
+      userId: req.session.user,
+    });
+    const reserved = await reserve.save();
+    const reservationId = reserved._id;
+    storeReserveID(reservationId);
+    service = "Riding";
+    res.render("customer/payment", { price: price });
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 //////////////////////////////////////////////////////////////////////////////////
 //--Post HORSE TRANNING reservation info to database
 //////////////////////////////////////////////////////////////////////////////////
-router.post('/training', async (req, res) => {
-  res.render("comming soon!");
+router.post("/training", async (req, res) => {
+  const date = req.body.days;
+  const formattedStart = moment(req.body.StartTime, ["h:mm A"]).format("HH:mm");
+  const formattedEnd = moment(req.body.EndTime, ["h:mm A"]).format("HH:mm");
+  const subStart = formattedStart.substring(0, 2);
+  const subEnd = formattedEnd.substring(0, 2);
+  const duration = subEnd - subStart;
+  const price = duration * 100;
+
+  //Remove for production
+  console.log("Start: " + formattedStart);
+  console.log("end: " + formattedEnd);
+  console.log("Date: " + date);
+  console.log("Duration: " + duration);
+  console.log("price: " + price);
+
+  try {
+    const reserve = new Training({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      phone: req.body.phone,
+      horseName: req.body.horsename,
+      days: date,
+      startTime: req.body.StartTime,
+      endTime: req.body.EndTime,
+      duration: duration,
+      paymentStatus: false,
+      paymentAmount: price,
+      userId: req.session.user,
+    });
+    const reserved = await reserve.save();
+    const reservationId = reserved._id;
+    storeReserveID(reservationId);
+    service = "Training";
+    res.render("customer/payment", { price: price });
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 //////////////////////////////////////////////////////////////////////////////////
 //--Post HORSE LODGING reservation info to database
 //////////////////////////////////////////////////////////////////////////////////
-router.post('/loding', async (req, res) => {
-  res.render("comming soon!");
+router.post("/loding", async (req, res) => {
+  const numDays = req.body.months * 30;
+  const horseType = req.body.horsetype;
+  const duration = req.body.months;
+  var price;
+
+  let startdate = new Date(req.body.startDay);
+  startdate = new Date(startdate.getTime() + startdate.getTimezoneOffset() * 60000);
+  let enddate = new Date(req.body.startDay);
+  enddate = new Date(enddate.getTime() + enddate.getTimezoneOffset() * 60000);
+  enddate.setDate(enddate.getDate() + numDays);
+  
+  if(horseType === "stable"){
+    price = duration*300;
+  }else if(horseType === "pasture"){
+    price = duration*250;
+  }
+
+  //Remove for production
+  console.log("StartDate: " + startdate);
+  console.log("EndDate: " + enddate);
+  console.log("Duration: " + duration);
+  console.log("Type: " + horseType);
+  console.log("Price: " + price);
+
+  try {
+    const reserve = new Lodging({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      phone: req.body.phone,
+      horseType: horseType,
+      startDay: startdate,
+      endDay: enddate,
+      duration: duration,
+      paymentStatus: false,
+      paymentAmount: price,
+      userId: req.session.user,
+    });
+    const reserved = await reserve.save();
+    const reservationId = reserved._id;
+    storeReserveID(reservationId);
+    service = "Lodging";
+    res.render("customer/payment", { price: price });
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -112,8 +195,27 @@ router.post("/payment", async (req, res) => {
   ////////////////////////////////////////////
   //--Get price amount
   ////////////////////////////////////////////
-  const reserPrice = await Reservation.findOne({ _id: getCurrId() });
-  const price = reserPrice.paymentAmount * 100;
+  var price;
+  switch (service) {
+    case SERVICE_TYPE.RIDING: {
+      const reserPrice = await Reservation.findOne({ _id: getCurrId() });
+      console.log("Price: " + reserPrice);
+      price = reserPrice.paymentAmount * 100;
+      break;
+    }
+    case SERVICE_TYPE.TRAINING: {
+      const reserPrice = await Training.findOne({ _id: getCurrId() });
+      console.log("Price: " + reserPrice);
+      price = reserPrice.paymentAmount * 100;
+      break;
+    }
+    case SERVICE_TYPE.LODGING: {
+      const reserPrice = await Lodging.findOne({ _id: getCurrId() });
+      console.log("Price: " + reserPrice);
+      price = reserPrice.paymentAmount * 100;
+      break;
+    }
+  }
 
   // Charge the customer's card
   const paymentsApi = client.paymentsApi;
@@ -162,17 +264,48 @@ router.post("/payment", async (req, res) => {
           paymentUrl: response.result.payment.receiptUrl,
         },
       };
-      Reservation.updateOne(
-        { _id: reservationId },
-        newvalues,
-        function (err, res) {
-          if (err) {
-            throw err;
-          } else {
-            console.log("Update successful");
-          }
+      switch (service) {
+        case SERVICE_TYPE.RIDING: {
+          Reservation.updateOne(
+            { _id: reservationId },
+            newvalues,
+            function (err, res) {
+              if (err) {
+                throw err;
+              } else {
+                console.log("Update successful");
+              }
+            }
+          );
+          break;
         }
-      );
+        case SERVICE_TYPE.TRAINING: {
+          Training.updateOne(
+            { _id: reservationId },
+            newvalues,
+            function (err, res) {
+              if (err) {
+                throw err;
+              } else {
+                console.log("Update successful");
+              }
+            }
+          );
+        }
+        case SERVICE_TYPE.LODGING: {
+          Lodging.updateOne(
+            { _id: reservationId },
+            newvalues,
+            function (err, res) {
+              if (err) {
+                throw err;
+              } else {
+                console.log("Update successful");
+              }
+            }
+          );
+        }
+      }
     } catch (error) {
       res.status(400).send("Payment Status Updated: " + error);
     }
---
+++ misc/service_type.js
@@ -0,0 +1,7 @@
+const SERVICE_TYPE = {
+    RIDING: 'Riding',
+    TRAINING: 'Training',
+    LODGING: 'Lodging'
+};
+
+module.exports = SERVICE_TYPE;
