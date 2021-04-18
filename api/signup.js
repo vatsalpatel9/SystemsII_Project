15352103ef559e43faa76778be946c1d3cf11045
vatsalpatel9 <44619923+vatsalpatel9@users.noreply.github.com>
2021-04-17 20:49:57 -0500

    Validate email and password on signup

--- api/signup.js
+++ api/signup.js
@@ -6,25 +6,36 @@ var router = express.Router();
 //create user
 router.post('/', async (req, res) => {
     try{
-        const password = req.body.password;
-        const cpassword = req.body.confirmpassword;
-        console.log(password, " ",cpassword);
-        if(password === cpassword){
-            const signUp = new Register({
-                firstname: req.body.firstname,
-                lastname: req.body.lastname,
-                email: req.body.email,
-                phone: req.body.phone,
-                password: bcrypt.hashSync(password, 10),
-                actype: 1
-            })
-            const registered = await signUp.save();
-            req.session.user = registered._id;
-            console.log(req.session.user);
-            res.redirect('/');
-        }else{
-            res.send("password are not matching")
-        }
+      const password = req.body.password;
+      const cpassword = req.body.confirmpassword;
+      const email = req.body.email;
+
+      // REGEX for email validation
+      if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+          console.log("emailcheckworking");
+        return res.redirect(
+           `/signup?errType=InvaildEmail`
+        );
+      }
+
+      console.log(password, " ", cpassword);
+
+      if (password === cpassword) {
+        const signUp = new Register({
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          email: email,
+          phone: req.body.phone,
+          password: bcrypt.hashSync(password, 10),
+          actype: 1,
+        });
+        const registered = await signUp.save();
+        req.session.user = registered._id;
+        console.log(req.session.user);
+        res.redirect("/");
+      } else {
+        return res.redirect("/signup?errType=PasswordMismatch");
+      }
     }catch (error){
         res.status(400).send(error)
     }
