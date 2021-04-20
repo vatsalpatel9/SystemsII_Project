37e1529005b4997c69cf35c00f30bbba960ded81
vatsalpatel9 <44619923+vatsalpatel9@users.noreply.github.com>
2021-04-20 18:19:17 -0500

    Added user reservation cancelation request.

--- api/cancelBookings.js
+++ api/cancelBookings.js
@@ -4,6 +4,7 @@ const Register = require("../models/registers");
 const Training = require("../models/horseTraining");
 const Lodging = require("../models/horseLodging");
 const refundModel = require("../models/refundModel");
+const Feedback = require("../models/feedback");
 const nodemailer = require("nodemailer");
 const router = express.Router();
 const SERVICE_TYPE = require("../misc/service_type");
@@ -19,6 +20,7 @@ const client = new Client({
 router.post("/adminCancel", async (req, res) => {
   const requestParams = req.body;
   const service = requestParams.service;
+  const userType = requestParams.userType;
   console.log(requestParams.idempotency_key);
   try {
     const response = await client.refundsApi.refundPayment({
@@ -91,37 +93,102 @@ router.post("/adminCancel", async (req, res) => {
     }
 
     //////////////////////////////////////////////////////////////////////////////
-    //--Mail Cancel request
+    //--Send Cancel request based on user type
     //////////////////////////////////////////////////////////////////////////////
     var userId = await Register.findOne({ _id: requestParams.userId });
-    var transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS,
-      },
-    });
+    const userName = userId.firstname;
+    const userEmail = userId.email;
+    const userPhone = userId.phone;
+    const refundAmount = response.result.refund.amountMoney.amount;
+    const refundId = response.result.refund.id;
+    switch (userType) {
+      case 'admin': {
+          //////////////////////////////////////////////////////////////////////////////
+          //--Mail ADMIN Cancel request
+          //////////////////////////////////////////////////////////////////////////////
+          var transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: process.env.EMAIL,
+              pass: process.env.PASS,
+            },
+          });
 
-    var mailOptions = {
-      from: process.env.EMAIL,
-      to: userId.email,
-      subject: "Reservation Canceled",
-      text: `Hello ${userId.firstname}, 
+          var mailOptions = {
+            from: process.env.EMAIL,
+            to: userEmail,
+            subject: "Reservation Canceled",
+            text: `Hello ${userName}, 
              Thank you for using Hoof Hearted Stables services. Sorry We have to cancel you
              reservation.\n
              Please contact us at CONTACT INFO regarding any question. We are sorry for the inconvience. \n
-             Your reservation cancel code is: ${response.result.refund.id}\n
-             You have been refunded $${response.result.refund.amountMoney.amount}\n
+             Your reservation cancel code is: ${refundId}\n
+             You have been refunded $${refundAmount}\n
              \nThank you for choosing Hoof Hearted Stables!`,
-    };
+          };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Email sent: " + info.response);
+            }
+          });
+        }
+        break;
+      case 'user': {
+        const message = `User ${userName} has canceled their ${service} reservation.\n
+                         A refund of $${refundAmount} was issued. Pending verification.\n
+                         The refund Id is: ${refundId}\n
+                         User contact is: ${userPhone}`;
+
+        const text = `Hello ${userName}, 
+             Thank you for using Hoof Hearted Stables services. Your ${service} reservation cancelation request have been recieved.\n
+             A refund will be issued with 3-5 business days.\n
+             Your reservation cancel code is: ${refundId}\n
+             You will be refunded $${refundAmount}\n
+             Please contact us at CONTACT INFO regarding any question.\n
+             Thank you for choosing Hoof Hearted Stables!`;
+
+        //////////////////////////////////////////////////////////////////////////////
+        //--Mail Cancel request confirmation
+        //////////////////////////////////////////////////////////////////////////////
+        var transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS,
+          },
+        });
+
+        var mailOptions = {
+          from: process.env.EMAIL,
+          to: userEmail,
+          subject: "Reservation Cancelation Request",
+          text: text,
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
+
+        //////////////////////////////////////////////////////////////////////////////
+        //--Mail USER Cancel request
+        //////////////////////////////////////////////////////////////////////////////
+        const cancelRequest = new Feedback({
+          name: userName,
+          email: userEmail,
+          subject: "Cancelation Request -- Auto Generated",
+          message: message,
+        });
+        await cancelRequest.save();
       }
-    });
+
+    }
 
     res.status(200).json({
       title: "Successfully Cancel",
